c08e4294d2c2af60ff50b223307d9be195ae7bfa
zaky aly <zaky3ly@gmail.com>
2022-05-29 21:21:06 +0200

    import contextProvider

--- src/App.js
+++ src/App.js
@@ -28,20 +28,23 @@ import {
   Employees,
 } from "./pages";
 
+import { useStatecontext } from "./contexts/ContextProvider";
+
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStatecontext();
+
   return (
     <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <div className="fixed right-4 bottom-4 " style={{ zIndex: "1000" }}>
             <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
-                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray cursor-pointer text-white rounded-full"
-                style={{ background: "tomato" }}
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray cursor-pointer text-white rounded-full "
+                style={{ background: "#FEE2E2" }}
               >
                 <FiSettings />
               </button>
