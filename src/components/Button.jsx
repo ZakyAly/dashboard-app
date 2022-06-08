704ba469413f4c38be780e2eca56bfdcc01557ab
zaky aly <zaky3ly@gmail.com>
2022-06-08 12:15:22 +0200

    fixing bug

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button = (color, bgColor, text, borderRadius, size) => {
+const Button = ({ color, bgColor, text, borderRadius, size }) => {
   return (
     <button
       type="button"
