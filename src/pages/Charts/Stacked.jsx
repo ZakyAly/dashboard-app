4dcd1560a9a76ba2a7a10b3b1490093b40182691
zaky aly <zaky3ly@gmail.com>
2022-06-11 09:46:55 +0200

    building Stacked chart

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import {
+  ChartComponent,
+  //   SeriesCollecttionDirective,
+  //   SeriesDirective,
+  //   Inject,
+  //   Legend,
+  //   Category,
+  //   StackingColumnSeries,
+  //   Tooltip,
+} from "@syncfusion/ej2-react-charts";
+// import { useStateContext } from "../../contexts/ContextProvider";
 
-const Stacked = () => {
-  return <div>Stacked</div>;
+import {
+  //   stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummyData";
+
+const Stacked = ({ width, height }) => {
+  //   const { state } = useStateContext();
+  return (
+    <ChartComponent
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      //   backgroun={currentMode === }
+      legendSettings={{ background: "white" }}
+    ></ChartComponent>
+  );
 };
 
 export default Stacked;
