6888f3c77dae4ca294228427de25a6434afed627
zaky aly <zaky3ly@gmail.com>
2022-06-16 22:54:14 +0200

    building ColorPicker page

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -3,6 +3,11 @@ import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
 
 import { Header } from "../components";
 
+const previewColor = (args) => {
+  document.getElementById("preview").style.backgroundColor =
+    args.currentValue.hex;
+};
+
 const ColorPicker = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
@@ -11,7 +16,26 @@ const ColorPicker = () => {
         <div id="preview" />
         <div className="flex justify-center items-center gap-20 flex-wrap">
           <div>
-            <p>Inline Pallete</p>
+            <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
+            <ColorPickerComponent
+              id="inline-pallete"
+              mode="Palette"
+              modeSwitcher={false}
+              showButtons={false}
+              inline
+              change={previewColor}
+            />
+          </div>
+          <div>
+            <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
+            <ColorPickerComponent
+              id="inline-picker"
+              mode="Picker"
+              modeSwitcher={false}
+              showButtons={false}
+              inline
+              change={previewColor}
+            />
           </div>
         </div>
       </div>
