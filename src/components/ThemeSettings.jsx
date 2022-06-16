6a859194e3398389b0fc7cf1183dc3c89855589e
zaky aly <zaky3ly@gmail.com>
2022-06-16 23:05:21 +0200

    building ThemeSettings

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -11,7 +11,7 @@ const ThemeSettings = () => {
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
       <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400 ">
         <div className="flex justify-between items-center p-4 ml-4">
-          <p className="font-semibold text-lg">Settings</p>
+          <p className="font-semibold text-xl">Settings</p>
           <button
             type="button"
             className="text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray rounded-full "
@@ -23,6 +23,37 @@ const ThemeSettings = () => {
           >
             <MdOutlineCancel />
           </button>
+        </div>
+        <div className="flex-col border-t-1 border-color ml-4 p-4">
+          <p className="font-semibold text-lg">Themes</p>
+          <div className="mt-4">
+            <input
+              className="cursor-pointer"
+              type="radio"
+              id="light"
+              name="theme"
+              value="Light"
+              onChange={() => {}}
+              checked={true}
+            />
+            <label className="ml-2 text-md cursor-pointer" htmlFor="light">
+              Light
+            </label>
+          </div>
+          <div className="mt-4">
+            <input
+              className="cursor-pointer"
+              type="radio"
+              id="dark"
+              name="theme"
+              value="Dark"
+              onChange={() => {}}
+              checked={true}
+            />
+            <label className="ml-2 text-md cursor-pointer" htmlFor="dark">
+              Dark
+            </label>
+          </div>
         </div>
       </div>
     </div>
