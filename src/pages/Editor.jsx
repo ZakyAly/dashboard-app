8f1c0a330435b01e29c1769841a44043d046f734
zaky aly <zaky3ly@gmail.com>
2022-06-16 16:16:12 +0200

    building text editor and replacing the EditorData with placeholder text

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+
+import { Header } from "../components";
 
 const Editor = () => {
-  return <div>Editor</div>;
+  const services = [Toolbar, Link, Image, HtmlEditor, QuickToolbar];
+
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Editor" />
+      <RichTextEditorComponent placeholder="Type here and start editing...">
+        <Inject services={services} />
+      </RichTextEditorComponent>
+    </div>
+  );
 };
 
 export default Editor;
