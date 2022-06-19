322761cf8c3f476c96e2753beb2276a78c2db02d
zaky aly <zaky3ly@gmail.com>
2022-06-19 13:38:42 +0200

    fixing margin top

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -22,7 +22,7 @@ const Customers = () => {
     allowAdding: true,
   };
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <div className="mt-24 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header title="Customers" category="Page" />
       <GridComponent
         dataSource={customersData}
