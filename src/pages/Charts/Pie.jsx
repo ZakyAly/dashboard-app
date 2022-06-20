7363d8faea8f5a407d271ede8a045fa2c956c509
zaky aly <zaky3ly@gmail.com>
2022-06-20 14:40:11 +0200

    building PieChart

---
+++ src/pages/Charts/Pie.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Header, PieChart } from "../../components";
+import { pieChartData } from "../../data/dummyData";
+
+const Pie = () => {
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Project Cost Breakdown" />
+      <div className="w-full">
+        <PieChart
+          id="chart-pie"
+          data={pieChartData}
+          legendVisiblity
+          height="full"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Pie;
